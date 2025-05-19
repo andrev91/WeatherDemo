cc33c63763de900fcd15f619ed72451de7612abf
andrev91 <andrevicente1991@gmail.com>
2025-05-18 23:52:45 -0400

    UI Adjustments

--- app/src/main/java/com/example/adventure/ui/theme/Theme.kt
+++ app/src/main/java/com/example/adventure/ui/theme/Theme.kt
@@ -9,28 +9,77 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkSkyBluePrimaryDark,
+    onPrimary = OnDarkSkyBluePrimaryDark,
+    primaryContainer = DarkSkyBluePrimaryContainerDark,
+    onPrimaryContainer = Color.Black,
+
+    secondary = LightBlueSecondaryDark,
+    onSecondary = OnLightBlueSecondaryDark,
+    secondaryContainer = LightBlueSecondaryContainerDark,
+    onSecondaryContainer = Color.White,
+
+    tertiary = YellowAccentTertiaryDark,
+    onTertiary = OnYellowAccentTertiaryDark,
+    tertiaryContainer = YellowAccentTertiaryContainerDark,
+    onTertiaryContainer = Color.Black,
+
+    error = ErrorDark,
+    onError = OnErrorDark,
+    errorContainer = Color(0xFFB3261E),
+    onErrorContainer = Color.White,
+
+    background = BackgroundDark,
+    onBackground = OnBackgroundDark,
+
+    surface = SurfaceDark,
+    onSurface = OnSurfaceDark,
+
+    surfaceVariant = Color(0xFF49454F),
+    onSurfaceVariant = OnSurfaceVariantDark,
+
+    outline = OutlineDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = SkyBluePrimaryLight,
+    onPrimary = OnSkyBluePrimaryLight,
+    primaryContainer = SkyBluePrimaryContainerLight,
+    onPrimaryContainer = OnSkyBluePrimaryLight,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = DeepBlueSecondaryLight,
+    onSecondary = OnDeepBlueSecondaryLight,
+    secondaryContainer = DeepBlueSecondaryContainerLight,
+    onSecondaryContainer = Color.Black,
+
+    tertiary = OrangeAccentTertiaryLight,
+    onTertiary = OnOrangeAccentTertiaryLight,
+    tertiaryContainer = OrangeAccentTertiaryContainerLight,
+    onTertiaryContainer = Color.Black,
+
+    error = ErrorLight,
+    onError = OnErrorLight,
+    errorContainer = Color(0xFFF9DEDC),
+    onErrorContainer = Color(0xFF410E0B),
+
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+
+    surface = SurfaceLight,
+    onSurface = OnSurfaceLight,
+
+    surfaceVariant = Color(0xFFE7E0EC),
+    onSurfaceVariant = OnSurfaceVariantLight,
+
+    outline = OutlineLight
 )
 
 @Composable
@@ -50,9 +99,19 @@ fun AdventureTheme(
         else -> LightColorScheme
     }
 
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = AppShapes,
         content = content
     )
 }
